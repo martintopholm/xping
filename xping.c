0eea1772ad23ce40d071bb2586cf770b80c6db43
Martin Topholm <mph@one.com>
2012-04-26 09:59:56 +0200

    IPv6 support.
    Split read_packet and write_packet in IPv4 and IPv6 versions.
    Have resolve try AAAA-records first and fall back to A record.
    Add flags to force IPv4 or IPv6 lookup.

--- xping.c
+++ xping.c
@@ -12,6 +12,8 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
+#include <netinet/ip6.h>
+#include <netinet/icmp6.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,24 +36,37 @@
 int	i_interval = 1000;
 int	a_flag = 0;
 int	A_flag = 0;
+int	v4_flag = 0;
+int	v6_flag = 0;
 
 /* Global structures */
+int	fd4;
+int	fd6;
 struct	event_base *ev_base;
 struct	evdns_base *dns;
 char	outpacket[IP_MAXPACKET];
+char	outpacket6[IP_MAXPACKET];
 int	datalen = 56;
 int	ident;
 
+#define ICMP6ECHOLEN 8
 #define NUM 300
 #define SETRES(t,i,r) t->res[(t->npkts+i) % NUM] = r
 #define GETRES(t,i) t->res[(t->npkts+i) % NUM]
+
+union addr {
+	struct sockaddr	sa;
+	struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+};
 
 SLIST_HEAD(slisthead, target) head = SLIST_HEAD_INITIALIZER(head);
 struct target {
 	char		host[64];
 	int		resolved;
+	int		evdns_type;
 
-	struct sockaddr_in sin;
+	union addr	sa;
 	int		npkts;
 	char		res[NUM+1];
 
@@ -71,24 +86,74 @@ struct statistics {
 static void redraw();
 static u_short in_cksum(u_short *, int);
 
+#define sa(x) ((struct sockaddr *)(&x->sa))
+#define sin(x) ((struct sockaddr_in *)(&x->sa))
+#define sin6(x) ((struct sockaddr_in6 *)(&x->sa))
 
-// struct in_addr *in_addrs = addresses;
-// struct in6_addr *in6_addrs = addresses;
+struct target *findtarget(int af, void *address)
+{
+	struct target *t;
+
+	if (af == AF_INET) {
+		SLIST_FOREACH(t, &head, entries) {
+			if (sa(t)->sa_family == AF_INET &&
+			    memcmp(&sin(t)->sin_addr,
+			    (struct in_addr *)address,
+			    sizeof(sin(t)->sin_addr)) == 0)
+				break;
+		}
+	} else if (af == AF_INET6) {
+		SLIST_FOREACH(t, &head, entries) {
+			if (sa(t)->sa_family == AF_INET6 &&
+			    memcmp(&sin6(t)->sin6_addr,
+			    (struct in6_addr *)address,
+			    sizeof(sin6(t)->sin6_addr)) == 0)
+				break;
+		}
+	} else {
+		return NULL;
+	}
+	return (t);
+}
+
 void resolved_host(int result, char type, int count, int ttl, void *addresses,
     void *thunk)
 {
 	struct target *t = thunk;
 
-	if (result == DNS_ERR_NONE && type == DNS_IPv4_A && count > 0) {
-		t->sin.sin_family = AF_INET;
-		memmove(&t->sin.sin_addr, (struct in_addr *)addresses,
-		    sizeof(t->sin.sin_addr));
-		t->resolved = 1;
+	/* libevent resolver may give NXDOMAIN for an AAAA query when
+	 * domain exists, but given RR doesn't. It appears when using
+	 * search domains. Resolver appears try search domains after
+	 * getting NOERROR with 0 answers.
+	 *
+	 * As a workaround assume that NXDOMAIN is the same as 0 answers
+	 * and try A lookup anyway unless we're in IPv6-only mode.
+	 */
+	if (result == DNS_ERR_NOTEXIST || result == DNS_ERR_NODATA) {
+		if (t->evdns_type == DNS_IPv6_AAAA && !v6_flag) {
+			t->evdns_type = DNS_IPv4_A;
+			evdns_base_resolve_ipv4(dns, t->host, 0,
+			    resolved_host, t);
+		}
 	}
 
+	if (result != DNS_ERR_NONE)
+		return;
+
+	if (type == DNS_IPv6_AAAA && count > 0) {
+		sin6(t)->sin6_family = AF_INET6;
+		memmove(&sin6(t)->sin6_addr, (struct in6_addr *)addresses,
+		    sizeof(sin6(t)->sin6_addr));
+		t->resolved = 1;
+	} else if (type == DNS_IPv4_A && count > 0) {
+		sin(t)->sin_family = AF_INET;
+		memmove(&sin(t)->sin_addr, (struct in_addr *)addresses,
+		    sizeof(sin(t)->sin_addr));
+		t->resolved = 1;
+	}
 }
 
-void read_packet(int fd, short what, void *thunk)
+void read_packet4(int fd, short what, void *thunk)
 {
 	struct target *t;
 	char inpacket[IP_MAXPACKET];
@@ -113,6 +178,9 @@ void read_packet(int fd, short what, void *thunk)
 
 	ip = (struct ip *)inpacket;
 	hlen = ip->ip_hl << 2;
+	if (ip->ip_p != IPPROTO_ICMP) {
+		return;
+	}
 	if (n < hlen + ICMP_MINLEN) {
 		stats->runt++;
 		return;
@@ -120,23 +188,19 @@ void read_packet(int fd, short what, void *thunk)
 
 	icp = (struct icmp *)(inpacket + hlen);
 	if (icp->icmp_type == ICMP_ECHOREPLY) {
-		if (icp->icmp_id != ident)
+		if (icp->icmp_id != htons(ident))
 			return; /*  skip other ping sessions */
 		seq = ntohs(icp->icmp_seq);
 
 		/* Search for our target */
-		SLIST_FOREACH(t, &head, entries) {
-			if (memcmp(&t->sin.sin_addr, &sin.sin_addr,
-			    sizeof(t->sin.sin_addr)) == 0) {
-				break;
-			}
-		}
+		t = findtarget(AF_INET, &sin.sin_addr);
 		if (t == NULL)
 			return; /* reply from unknown src */
 
 		/* XXX Checksum is propably verified by host OS */
 		t->res[seq % NUM] = '.';
-		if (a_flag) write(STDOUT_FILENO, "\a", 1);
+		if (a_flag)
+			write(STDOUT_FILENO, "\a", 1);
 		stats->received++;
 	} else {
 		/* Check aspects of the original packet */
@@ -146,17 +210,12 @@ void read_packet(int fd, short what, void *thunk)
 			return;
 		if (oicp->icmp_type != ICMP_ECHO)
 			return;
-		if (oicp->icmp_id != ident)
+		if (oicp->icmp_id != htons(ident))
 			return;
 		seq = ntohs(oicp->icmp_seq);
 
 		/* Search for our target */
-		SLIST_FOREACH(t, &head, entries) {
-			if (memcmp(&t->sin.sin_addr, &oip->ip_dst,
-			    sizeof(t->sin.sin_addr)) == 0) {
-				break;
-			}
-		}
+		t = findtarget(AF_INET, &oip->ip_dst);
 		if (t == NULL)
 			return; /* original target is unknown */
 
@@ -170,29 +229,80 @@ void read_packet(int fd, short what, void *thunk)
 	redraw();
 }
 
-void write_packet(int fd, short what, void *thunk)
+void read_packet6(int fd, short what, void *thunk)
+{
+	struct target *t = thunk;
+	char inpacket[IP_MAXPACKET];
+	struct sockaddr_in6 sin6;
+	struct ip6_hdr *oip6;
+	struct icmp6_hdr *icmp6h;
+	struct icmp6_hdr *oicmp6h;
+	socklen_t salen;
+	int seq;
+	int n;
+
+	salen = sizeof(sin6);
+	memset(inpacket, 0, sizeof(inpacket));
+	n = recvfrom(fd, inpacket, sizeof(inpacket), 0,
+	    (struct sockaddr *)&sin6, &salen);
+	if (n < 0) {
+		stats->recvfrom_err++;
+		return;
+	}
+	if (n < ICMP6ECHOLEN) {
+		stats->runt++;
+		return;
+	}
+
+	/* SOCK_RAW for IPPROTO_ICMPV6 doesn't include IPv6 header */
+	icmp6h = (struct icmp6_hdr *)(inpacket);
+	if (icmp6h->icmp6_type == ICMP6_ECHO_REPLY) {
+		if (icmp6h->icmp6_id != htons(ident))
+			return; /*  skip other ping sessions */
+		seq = ntohs(icmp6h->icmp6_seq);
+
+		/* Search for our target */
+		t = findtarget(AF_INET6, &sin6.sin6_addr);
+		if (t == NULL)
+			return; /* reply from unknown src */
+
+		/* XXX Checksum is propably verified by host OS */
+		t->res[seq % NUM] = '.';
+		if (a_flag)
+			write(STDOUT_FILENO, "\a", 1);
+		stats->received++;
+	} else {
+		/* Check aspects of the original packet */
+		oip6 = (struct ip6_hdr *)(icmp6h + 1);
+		oicmp6h = (struct icmp6_hdr *)(oip6 + 1);
+		if (oip6->ip6_nxt != IPPROTO_ICMPV6)
+			return;
+		if (oicmp6h->icmp6_type != ICMP6_ECHO_REQUEST)
+			return;
+		if (oicmp6h->icmp6_id != htons(ident))
+			return;
+		seq = ntohs(oicmp6h->icmp6_seq);
+
+		/* Search for our target */
+		t = findtarget(AF_INET6, &oip6->ip6_dst);
+		if (t == NULL)
+			return; /* original target is unknown */
+
+		if (icmp6h->icmp6_type == ICMP6_DST_UNREACH)
+			t->res[seq % NUM] = '#';
+		else
+			t->res[seq % NUM] = '%';
+		stats->other++;
+	}
+
+	redraw();
+}
+
+int write_packet4(int fd, short what, void *thunk)
 {
 	struct target *t = thunk;
 	struct icmp *icp;
 	int len;
-	int n;
-
-	/* Register unresolved missed packets */
-	if (!t->resolved) {
-		if (t->npkts > 0) {
-			SETRES(t, -1, '@');
-		}
-		SETRES(t, 0, '@');
-		t->npkts++;
-		redraw();
-		return;
-	}
-
-	/* Register missed reply */
-	if (t->npkts > 0 && GETRES(t, -1) == ' ') {
-		SETRES(t, -1, '?');
-		if (A_flag) write(STDOUT_FILENO, "\a", 1);
-	}
 
 	/* Send packet */
 	len = ICMP_MINLEN + datalen;
@@ -201,12 +311,52 @@ void write_packet(int fd, short what, void *thunk)
 	icp->icmp_code = 0;
 	icp->icmp_cksum = 0;
 	icp->icmp_seq = htons(t->npkts);
-	icp->icmp_id = ident;
+	icp->icmp_id = htons(ident);
 	icp->icmp_cksum = in_cksum((u_short *)icp, len);
 	SETRES(t, 0, ' ');
 
-	n = sendto(fd, outpacket, len, 0, (struct sockaddr *)&t->sin,
-            sizeof(t->sin));
+	return sendto(fd, outpacket, len, 0, sa(t), sizeof(struct sockaddr_in));
+}
+
+int write_packet6(int fd, short what, void *thunk)
+{
+	struct target *t = thunk;
+	struct icmp6_hdr *icmp6h;
+	int len;
+
+	len = ICMP6ECHOLEN + datalen;
+	icmp6h = (struct icmp6_hdr *)outpacket6;
+	icmp6h->icmp6_type = ICMP6_ECHO_REQUEST;
+	icmp6h->icmp6_code = 0;
+	icmp6h->icmp6_cksum = 0;
+	icmp6h->icmp6_seq = htons(t->npkts);
+	icmp6h->icmp6_id = htons(ident);
+	return sendto(fd, outpacket6, len, 0, sa(t), sizeof(struct sockaddr_in6));
+}
+
+void write_packet(int fd, short what, void *thunk)
+{
+	struct target *t = thunk;
+	int len;
+	int n;
+
+	/* Register missed reply */
+	if (t->npkts > 0 && GETRES(t, -1) == ' ') {
+		SETRES(t, -1, '?');
+		if (A_flag)
+			write(STDOUT_FILENO, "\a", 1);
+	}
+
+	/* Send protocol packet */
+	if (sa(t)->sa_family == AF_INET6) {
+		n = write_packet6(fd6, what, thunk);
+		len = ICMP6ECHOLEN + datalen;
+	} else {
+		n = write_packet4(fd4, what, thunk);
+		len = ICMP_MINLEN + datalen;
+	}
+
+	/* Register transmit result */
 	if (n < 0) {
 		stats->sendto_err++;
 		SETRES(t, 0, '$'); /* transmit error */
@@ -222,13 +372,34 @@ void write_packet(int fd, short what, void *thunk)
 
 void write_first_packet(int fd, short what, void *thunk)
 {
+	struct target *t = thunk;
 	struct event *ev;
 	struct timeval tv;
 
-	write_packet(fd, what, thunk);
 	tv.tv_sec = i_interval / 1000;
 	tv.tv_usec = i_interval % 1000 * 1000;
-	ev = event_new(ev_base, fd, EV_PERSIST, write_packet, thunk);
+
+	/* Register unresolved missed packets */
+	if (!t->resolved) {
+		ev = event_new(ev_base, fd4, 0, write_first_packet, thunk);
+		event_add(ev, &tv);
+		if (t->npkts > 0) {
+			SETRES(t, -1, '@');
+		}
+		SETRES(t, 0, '@');
+		t->npkts++;
+		redraw();
+		return;
+	}
+
+	/* Schedule targets on proper socket */
+	if (sa(t)->sa_family == AF_INET6) {
+		write_packet(fd6, what, thunk);
+		ev = event_new(ev_base, fd6, EV_PERSIST, write_packet, t);
+	} else {
+		write_packet(fd4, what, thunk);
+		ev = event_new(ev_base, fd4, EV_PERSIST, write_packet, t);
+	}
 	event_add(ev, &tv);
 }
 
@@ -241,7 +412,8 @@ void redraw()
 	int i, imax, ifirst, ilast;
 
 	t = SLIST_FIRST(&head);
-	if (t == NULL) return;
+	if (t == NULL)
+		return;
 
 	col = getmaxx(stdscr);
 	imax = MIN(t->npkts, col - 20);
@@ -257,8 +429,10 @@ void redraw()
 	SLIST_FOREACH(t, &head, entries) {
 		mvprintw(y, 0, "%19.19s ", t->host);
 		for (i=ifirst; i<ilast; i++) {
-			if (i < t->npkts) addch(t->res[i % NUM]);
-			else addch(' ');
+			if (i < t->npkts)
+				addch(t->res[i % NUM]);
+			else
+				addch(' ');
 		}
 		y++;
 	}
@@ -280,13 +454,13 @@ void redraw()
 
 void usage(const char *whine)
 {
-        if (whine != NULL) {
-                fprintf(stderr, "%s\n", whine);
-        }
+	if (whine != NULL) {
+		fprintf(stderr, "%s\n", whine);
+	}
 	fprintf(stderr,
 	    "usage: xping [-AVh] [-i interval] host [host [...]]\n"
 	    "\n");
-        exit(EX_USAGE);
+	exit(EX_USAGE);
 }
 
 int main(int argc, char *argv[])
@@ -296,21 +470,33 @@ int main(int argc, char *argv[])
 	struct target *t;
 	char *end;
 	int salen;
-	int fd;
 	int i;
 	char ch;
 
 	/* Open RAW-socket and drop root-privs */
-	fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	fd4 = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	fd6 = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
 	setuid(getuid());
-	if (fd < 0) {
+	if (fd4 < 0) {
 		perror("socket");
+		return 1;
+	}
+	if (fd6 < 0) {
+		perror("socket (IPv6)");
 		return 1;
 	}
 
 	/* Parse command line options */
-	while ((ch = getopt(argc, argv, "Aai:hV")) != -1) {
+	while ((ch = getopt(argc, argv, "46Aai:hV")) != -1) {
 		switch(ch) {
+		case '4':
+			v4_flag = 1;
+			v6_flag = 0;
+			break;
+		case '6':
+			v4_flag = 0;
+			v6_flag = 1;
+			break;
 		case 'a':
 			a_flag = 1;
 			break;
@@ -344,56 +530,67 @@ int main(int argc, char *argv[])
 	/* Prepare statistics and datapacket */
 	stats = &statistics;
 	memset(stats, 0, sizeof(stats));
-	ident = htons(getpid() & 0xffff);
+	ident = getpid() & 0xffff;
 	for (i=0; i<datalen; i++) {
 		outpacket[ICMP_MINLEN + i] = '0' + i;
+		outpacket6[ICMP6ECHOLEN + i] = '0' + i;
 	}
 
 	/* Prepare event system and inbound socket */
 	ev_base = event_base_new();
 	dns = evdns_base_new(ev_base, 1);
-	evutil_make_socket_nonblocking(fd);
-	ev = event_new(ev_base, fd, EV_READ|EV_PERSIST,
-	    read_packet, NULL);
+	evutil_make_socket_nonblocking(fd4);
+	ev = event_new(ev_base, fd4, EV_READ|EV_PERSIST, read_packet4, NULL);
+	event_add(ev, NULL);
+	evutil_make_socket_nonblocking(fd6);
+	ev = event_new(ev_base, fd6, EV_READ|EV_PERSIST, read_packet6, NULL);
 	event_add(ev, NULL);
 
-	/* Add and schedule targets */
+	/* Add and resolve targets */
 	SLIST_INIT(&head);
 	for (i=argc-1; i>=0; i--) {
 		t = malloc(sizeof(*t));
 		memset(t, 0, sizeof(*t));
 		memset(t->res, ' ', sizeof(t->res));
-
 		strncat(t->host, argv[i], sizeof(t->host) - 1);
 		SLIST_INSERT_HEAD(&head, t, entries);
+
+		salen = sizeof(t->sa);
+		if (evutil_parse_sockaddr_port(t->host, sa(t), &salen) == 0) {
+			t->resolved = 1;
+		} else {
+			if (v4_flag) {
+				t->evdns_type = DNS_IPv4_A;
+				evdns_base_resolve_ipv4(dns, t->host, 0,
+				    resolved_host, t);
+			} else {
+				t->evdns_type = DNS_IPv6_AAAA;
+				evdns_base_resolve_ipv6(dns, t->host, 0,
+				    resolved_host, t);
+			}
+		}
 	}
+	/* Schedule and resolve targets */
 	tv.tv_sec = 0;
 	tv.tv_usec = 0;
 	SLIST_FOREACH(t, &head, entries) {
-		ev = event_new(ev_base, fd, 0, write_first_packet, t);
+		if (sa(t)->sa_family == AF_INET6) {
+			ev = event_new(ev_base, fd6, 0, write_first_packet, t);
+		} else {
+			ev = event_new(ev_base, fd4, 0, write_first_packet, t);
+		}
 		event_add(ev, &tv);
 		tv.tv_usec += 100*1000; /* target spacing: 100ms */
 		tv.tv_sec += (tv.tv_usec >= 1000000 ? 1 : 0);
 		tv.tv_usec -= (tv.tv_usec >= 1000000 ? 1000000 : 0);
 	}
 
-	/* Resolve hostnames */
-	SLIST_FOREACH(t, &head, entries) {
-		salen = sizeof(t->sin);
-		if (evutil_parse_sockaddr_port(t->host,
-		    (struct sockaddr *)&t->sin, &salen) == 0) {
-			t->resolved = 1;
-		} else {
-			evdns_base_resolve_ipv4(dns, t->host, 0,
-			    resolved_host, t);
-		}
-	}
-
 	initscr();
 	event_base_dispatch(ev_base);
 	endwin();
 
-	close(fd);
+	close(fd4);
+	close(fd6);
 	return 0;
 }
 
