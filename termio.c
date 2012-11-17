martintopholm/xping

commit 104e3722a1b0c6a16f29069b02d1d6aaec149b81

ui: split terminal output in seperate file
Split termio.c on compile time option NCURSES.
The ncurses library should now be optional.
Build with vt100 control characters as default.

termio.c
@@ -1,0 +1,168 @@
+/*-
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <mph@hoth.dk> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Martin Topholm
+ * ----------------------------------------------------------------------------
+ */
+
+#include <sys/param.h>
+#include <sys/ioctl.h>
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef NCURSES
+#define stdscr
+#else /* NCURSES */
+#include <ncurses.h>
+#endif /* !NCURSES */
+
+#include "xping.h"
+
+#ifndef NCURSES
+int
+getmaxx(void)
+{
+	struct winsize wsz;
+	char *p;
+
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsz) != -1 &&
+	    wsz.ws_col > 0)
+		return (wsz.ws_col);
+	else if ((p = getenv("COLUMNS")) != NULL && *p != '\0')
+		return atoi(p);
+	else
+		return 0;
+}
+
+void
+move(int row, int col)
+{
+	fprintf(stdout, "[%d;%dH", row+1, col);
+}
+
+static void
+clrtobot(void)
+{
+	fprintf(stdout, "[J");
+}
+
+static void
+clrtoeol(void)
+{
+	fprintf(stdout, "[K");
+}
+
+static void
+addch(int ch)
+{
+	fputc(ch, stdout);
+}
+
+static void
+mvprintw(int row, int col, const char *fmt, ...)
+{
+	va_list ap;
+
+	move(row, col);
+	fprintf(stdout, "[K");
+	va_start(ap, fmt);
+	vfprintf(stdout, fmt, ap);
+	va_end(ap);
+}
+
+static void
+refresh(void)
+{
+	fflush(stdout);
+}
+#endif
+
+/*
+ * Prepares the terminal for drawing
+ */
+void
+termio_init(void)
+{
+#ifndef NCURSES
+	move(0, 0);
+	clrtobot();
+#else /* NCURSES */
+	initscr();
+#endif /* !NCURSES */
+}
+
+/*
+ * Draws the recorded replies on the terminal.
+ */
+void
+termio_update(void)
+{
+	struct target *t;
+	int col;
+	int y;
+
+	int i, imax, ifirst, ilast;
+
+	t = SLIST_FIRST(&head);
+	if (t == NULL)
+		return;
+
+	col = getmaxx(stdscr);
+	imax = MIN(t->npkts, col - 20);
+	imax = MIN(imax, NUM);
+	ifirst = (t->npkts > imax ? t->npkts - imax : 0);
+	ilast = t->npkts;
+
+	move(0, 0);
+	clrtoeol();
+	mvprintw(0, col/2 - (8+strlen(version)+strlen(built))/2,
+	    "xping [%s]", version);
+
+	y = 2;
+	SLIST_FOREACH(t, &head, entries) {
+		mvprintw(y, 0, "%19.19s ", t->host);
+		if (t->duplicate != NULL)
+			mvprintw(y, 20, "(duplicate of %s)", t->duplicate->host);
+		else {
+			for (i=ifirst; i<ilast; i++) {
+				if (i < t->npkts)
+					addch(t->res[i % NUM]);
+				else
+					addch(' ');
+			}
+		}
+		y++;
+	}
+
+	y++;
+	mvprintw(y++, 0, "Sent: %d", stats->transmitted);
+	mvprintw(y++, 0, "Recv: %d", stats->received);
+	mvprintw(y++, 0, "ErrO: %d", stats->sendto_err);
+	mvprintw(y++, 0, "ErrI: %d", stats->recvfrom_err);
+	mvprintw(y++, 0, "Runt: %d", stats->runt);
+	mvprintw(y++, 0, "Othr: %d", stats->other);
+	y++;
+	mvprintw(y++, 0, "Legend  . echo-reply   ? timeout      # unreach    "
+	    "%%=other");
+	mvprintw(y++, 0, "        @ resolving    ! send-error");
+	move(y++, 0);
+	clrtobot();
+
+	refresh();
+}
+
+/*
+ * Clean up screen and restore old modes
+ */
+void
+termio_cleanup(void)
+{
+#ifndef NCURSES
+#else /* NCURSES */
+	endwin();
+#endif /* !NCURSES */
+}

xping.c
@@ -9,7 +9,6 @@
 
 #include <sys/param.h>
 
-#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip6.h>
@@ -23,13 +22,8 @@
 
 #include <event2/event.h>
 #include <event2/dns.h>
-#include <ncurses.h>
 
-#include "queue.h"
-#include "uthash.h"
-
-extern const char version[];
-extern const char built[];
+#include "xping.h"
 
 /* Option flags */
 int	i_interval = 1000;
@@ -48,49 +42,17 @@ char	outpacket6[IP_MAXPACKET];
 int	datalen = 56;
 int	ident;
 
-#define ICMP6_MINLEN sizeof(struct icmp6_hdr)
-#define NUM 300
+struct target *hash = NULL;
+struct slisthead head = SLIST_HEAD_INITIALIZER(head);
+
 #define SETRES(t,i,r) t->res[(t->npkts+i) % NUM] = r
 #define GETRES(t,i) t->res[(t->npkts+i) % NUM]
 
-union addr {
-	struct sockaddr	sa;
-	struct sockaddr_in sin;
-	struct sockaddr_in6 sin6;
-};
-
-SLIST_HEAD(slisthead, target) head = SLIST_HEAD_INITIALIZER(head);
-struct target *hash = NULL;
-struct target {
-	char		host[64];
-	int		resolved;
-	int		evdns_type;
-
-	union addr	sa;
-	int		npkts;
-	char		res[NUM+1];
-
-	struct target	*duplicate;
-	SLIST_ENTRY(target) entries;
-	UT_hash_handle	hh;
-};
-
-struct statistics {
-	int		transmitted;
-	int		received;
-
-	int		sendto_err;
-	int		recvfrom_err;
-	int		runt;
-	int		other;
-} statistics, *stats;
-
-static void redraw();
 static u_short in_cksum(u_short *, int);
 
-#define sa(x) ((struct sockaddr *)(&x->sa))
-#define sin(x) ((struct sockaddr_in *)(&x->sa))
-#define sin6(x) ((struct sockaddr_in6 *)(&x->sa))
+void (*init)(void) = termio_init;
+void (*update)(void) = termio_update;
+void (*cleanup)(void) = termio_cleanup;
 
 /*
  * Insert a new target into the hash table. Mark as a duplicate if the
@@ -253,7 +215,7 @@ read_packet4(int fd, short what, void *thunk)
 		}
 		stats->other++;
 	}
-	redraw();
+	update();
 }
 
 void
@@ -327,7 +289,7 @@ read_packet6(int fd, short what, void *thunk)
 		stats->other++;
 	}
 
-	redraw();
+	update();
 }
 
 int
@@ -409,7 +371,7 @@ write_packet(int fd, short what, void *thunk)
 	}
 	stats->transmitted++;
 	t->npkts++;
-	redraw();
+	update();
 }
 
 /*
@@ -435,7 +397,7 @@ write_first_packet(int fd, short what, void *thunk)
 		}
 		SETRES(t, 0, '@');
 		t->npkts++;
-		redraw();
+		update();
 		return;
 	}
 
@@ -448,66 +410,6 @@ write_first_packet(int fd, short what, void *thunk)
 		ev = event_new(ev_base, fd4, EV_PERSIST, write_packet, t);
 	}
 	event_add(ev, &tv);
-}
-
-/*
- * Draws the recorded replies on the terminal.
- */
-void
-redraw()
-{
-	struct target *t;
-	int col;
-	int y;
-
-	int i, imax, ifirst, ilast;
-
-	t = SLIST_FIRST(&head);
-	if (t == NULL)
-		return;
-
-	col = getmaxx(stdscr);
-	imax = MIN(t->npkts, col - 20);
-	imax = MIN(imax, NUM);
-	ifirst = (t->npkts > imax ? t->npkts - imax : 0);
-	ilast = t->npkts;
-
-	move(0, 0);
-	clrtoeol();
-	mvprintw(0, col/2 - (8+strlen(version)+strlen(built))/2,
-	    "xping [%s]", version);
-
-	y = 2;
-	SLIST_FOREACH(t, &head, entries) {
-		mvprintw(y, 0, "%19.19s ", t->host);
-		if (t->duplicate != NULL)
-			mvprintw(y, 20, "(duplicate of %s)", t->duplicate->host);
-		else {
-			for (i=ifirst; i<ilast; i++) {
-				if (i < t->npkts)
-					addch(t->res[i % NUM]);
-				else
-					addch(' ');
-			}
-		}
-		y++;
-	}
-
-	y++;
-	mvprintw(y++, 0, "Sent: %d", stats->transmitted);
-	mvprintw(y++, 0, "Recv: %d", stats->received);
-	mvprintw(y++, 0, "ErrO: %d", stats->sendto_err);
-	mvprintw(y++, 0, "ErrI: %d", stats->recvfrom_err);
-	mvprintw(y++, 0, "Runt: %d", stats->runt);
-	mvprintw(y++, 0, "Othr: %d", stats->other);
-	y++;
-	mvprintw(y++, 0, "Legend  . echo-reply   ? timeout      # unreach   "
-	    "%% other");
-	mvprintw(y++, 0, "        @ resolving    ! send-error");
-
-	move(y++, 0);
-
-	refresh();
 }
 
 void
@@ -653,9 +555,9 @@ main(int argc, char *argv[])
 		tv.tv_usec -= (tv.tv_usec >= 1000000 ? 1000000 : 0);
 	}
 
-	initscr();
+	init();
 	event_base_dispatch(ev_base);
-	endwin();
+	cleanup();
 
 	close(fd4);
 	close(fd6);

Makefile
@@ -9,7 +9,7 @@ SBINPATH=$(PREFIX)/bin
 MANPATH=$(PREFIX)/man
 CFLAGS=-Wall -Werror -I./$(LIBEVENT) -I./$(LIBEVENT)/include
 LDFLAGS=-L./$(LIBEVENT)/.libs
-LIBS=libevent.a -lrt -lcurses
+LIBS=libevent.a -lrt
 VERSION="`git describe --tags --always --dirty=+ 2>/dev/null || date +snapshot-%Y%m%dT%H%M%S`"
 TIMESTAMP="`date +%Y%m%dT%H%M%S`"
 
@@ -24,26 +24,16 @@ libevent.a:
 	cp ./$(LIBEVENT)/.libs/libevent.a .
 	size libevent.a
 
-curses.so:
-	printf "#include <ncurses.h>\nint main() { initscr(); return 0; }" | \
-            gcc -x c -o /dev/null - -lcurses && touch curses.so
-
 version.o:
 	(printf "const char version[] = \"%s\";\n" $(VERSION); \
 	 printf "const char built[] = \"%s\";\n" $(TIMESTAMP)) | \
 	 gcc -x c -c -o version.o -
 
-xping: libevent.a curses.so xping.o version.o
-	$(CC) $(LDFLAGS) -g -o xping xping.o version.o $(LIBS)
+xping: libevent.a xping.o termio.o version.o
+	$(CC) $(LDFLAGS) -g -o xping xping.o termio.o version.o $(LIBS)
 
 xping.8.gz: xping.8
 	gzip < xping.8 > xping.8.gz
-
-xping.8.txt: xping.8
-	groff -mman -Tascii xping.8 | sed 's/.//g' > xping.8.txt
-
-xping.8.html: xping.8
-	-groff -mman -Thtml xping.8 > xping.8.html
 
 install:
 	mkdir -p $(SBINPATH)
@@ -52,4 +42,4 @@ install:
 	install -m 444 xping.8.gz $(MANPATH)/man8/
 
 clean:
-	rm -f xping xping.o xping.8.gz xping.8.txt xping.8.html version.o curses.so
+	rm -f xping xping.o xping.8.gz termio.o version.o

no-ncurses.patch
@@ -1,134 +1,0 @@
-Index: xping.c
-===================================================================
---- xping.c	(revision 12200)
-+++ xping.c	(working copy)
-@@ -8,11 +8,13 @@
-  */
- 
- #include <sys/param.h>
-+#include <sys/ioctl.h>
- 
- #include <netinet/in.h>
- #include <netinet/ip.h>
- #include <netinet/ip_icmp.h>
- 
-+#include <stdarg.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
-@@ -21,7 +23,7 @@
- 
- #include <event2/event.h>
- #include <event2/dns.h>
--#include <ncurses.h>
-+//#include <ncurses.h>
- 
- #include "queue.h"
- 
-@@ -218,15 +220,43 @@
- 	redraw();
- }
- 
-+void vt100clear()
-+{
-+	fprintf(stdout, "[2J");
-+}
-+
-+void vt100print(int row, int col, const char *fmt, ...)
-+{
-+	va_list ap;
-+
-+	fprintf(stdout, "[%d;%dH", row, col);
-+	fprintf(stdout, "[K");
-+	va_start(ap, fmt);
-+	vfprintf(stdout, fmt, ap);
-+	va_end(ap);
-+}
-+
-+void vt100addch(int ch)
-+{
-+	fputc(ch, stdout);
-+}
-+
- void redraw()
- {
- 	struct target *t;
--	int row, col;
-+	struct winsize wsz;
-+	char *p;
-+	int col;
- 	int y;
- 
- 	int i, imax, ifirst, ilast;
- 
--	getmaxyx(stdscr,row,col);
-+	if ((p = getenv("COLUMNS")) != NULL && *p != '\0')
-+		col = atoi(p);
-+	else if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &wsz) != -1 &&
-+	    wsz.ws_col > 0)
-+		col = wsz.ws_col - 2;
-+
- 	t = SLIST_FIRST(&head);
- 	if (t == NULL) return;
- 
-@@ -235,33 +265,29 @@
- 	ifirst = (t->npkts > imax ? t->npkts - imax : 0);
- 	ilast = t->npkts;
- 
--	move(0, 0);
--	clrtoeol();
--	mvprintw(0, col/2 - strlen(VERSION)/2, "%s", VERSION);
--
-+	vt100print(0, col/2 - strlen(VERSION)/2, "%s", VERSION);
- 	y = 2;
- 	SLIST_FOREACH(t, &head, entries) {
--		mvprintw(y, 0, "%19.19s ", t->host);
-+		vt100print(y, 0, "%19.19s ", t->host);
- 		for (i=ifirst; i<ilast; i++) {
--			if (i < t->npkts) addch(t->res[i % NUM]);
--			else addch(' ');
-+			if (i < t->npkts) vt100addch(t->res[i % NUM]);
-+			else vt100addch(' ');
- 		}
- 		y++;
- 	}
- 
- 	y++;
--	mvprintw(y++, 0, "Sent: %d", stats->transmitted);
--	mvprintw(y++, 0, "Recv: %d", stats->received);
--	mvprintw(y++, 0, "ErrO: %d", stats->sendto_err);
--	mvprintw(y++, 0, "ErrI: %d", stats->recvfrom_err);
--	mvprintw(y++, 0, "Runt: %d", stats->runt);
--	mvprintw(y++, 0, "Othr: %d", stats->other);
-+	vt100print(y++, 0, "Sent: %d", stats->transmitted);
-+	vt100print(y++, 0, "Recv: %d", stats->received);
-+	vt100print(y++, 0, "ErrO: %d", stats->sendto_err);
-+	vt100print(y++, 0, "ErrI: %d", stats->recvfrom_err);
-+	vt100print(y++, 0, "Runt: %d", stats->runt);
-+	vt100print(y++, 0, "Othr: %d", stats->other);
- 	y++;
--	mvprintw(y++, 0, "Legend recv: .=echoreply ?=noreply #=unreach %=other"); 
--	mvprintw(y++, 0, "       send: @=resolving !=partial $=other");
--	move(y++, 0);
--
--	refresh();
-+	vt100print(y++, 0, "Legend recv: .=echoreply ?=noreply #=unreach %=other"); 
-+	vt100print(y++, 0, "       send: @=resolving !=partial $=other");
-+	vt100print(y++, 0, "");
-+	fflush(stdout);
- }
- 
- void usage(const char *whine)
-@@ -370,9 +396,10 @@
- 		}
- 	}
- 
--	initscr();
-+	//initscr();
-+	vt100clear();
- 	event_base_dispatch(ev_base);
--	endwin();
-+	//endwin();
- 
- 	close(fd);
- 	return 0;

xping.h
@@ -1,0 +1,58 @@
+#ifndef XPING_H
+#define XPING_H
+
+#include <netinet/in.h>
+
+#include "queue.h"
+#include "uthash.h"
+
+
+#define ICMP6_MINLEN sizeof(struct icmp6_hdr)
+#define NUM 300
+
+extern SLIST_HEAD(slisthead, target) head;
+
+union addr {
+	struct sockaddr sa;
+	struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+};
+
+struct target {
+	char		host[64];
+	int		resolved;
+	int		evdns_type;
+
+	union addr	sa;
+	int		npkts;
+	char		res[NUM+1];
+
+	struct target	*duplicate;
+	SLIST_ENTRY(target) entries;
+	UT_hash_handle	hh;
+};
+
+struct statistics {
+	int		transmitted;
+	int		received;
+
+	int		sendto_err;
+	int		recvfrom_err;
+	int		runt;
+	int		other;
+} statistics, *stats;
+
+#define sa(x) ((struct sockaddr *)(&x->sa))
+#define sin(x) ((struct sockaddr_in *)(&x->sa))
+#define sin6(x) ((struct sockaddr_in6 *)(&x->sa))
+
+/* from "version.c" */
+extern const char version[];
+extern const char built[];
+
+/* from termio.c */
+void termio_init(void);
+void termio_update(void);
+void termio_cleanup(void);
+
+#endif /* !XPING_H */

commit 931ba412f018f9dd026917108953f07ad746507d

bug: header dependencies
FreeBSD needed "in.h" before "ip.h" - a regression from 104e372. Also
termio.c needs to know AF_INET and AF_INET6 declared in sys/socket.h
in FreeBSD.

termio.c
@@ -9,6 +9,7 @@
 
 #include <sys/param.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 
 #include <stdarg.h>
 #include <stdio.h>

xping.c
@@ -9,6 +9,7 @@
 
 #include <sys/param.h>
 
+#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/ip6.h>