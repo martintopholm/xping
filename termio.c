martintopholm/xping

commit 94f862696eea4ed23db0355a90f248d56ad4e58e

main: split struct target into main/module parts
Instead of having xping.c own all data let a probe define its own struct
and ask the probe to report just results back to xping.c

Duplicate reporting (icmp.c) is the responsibility of the module.

termio.c
@@ -174,25 +174,21 @@ updatefull(int ifirst, int ilast)
 	row = 0;
 	DL_FOREACH(list, t) {
 		t->row = row; /* cache for selective updates */
-		if (C_flag && t->af && sa(t)->sa_family == AF_INET6)
+		if (C_flag && t->af == AF_INET6)
 			mvprintw(row, 0, "%c[2;32m%*.*s%c[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
-		else if (C_flag && t->af && sa(t)->sa_family == AF_INET)
+		else if (C_flag && t->af == AF_INET)
 			mvprintw(row, 0, "%c[2;31m%*.*s%c[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
 		else
 			mvprintw(row, 0, "%*.*s", w_width, w_width, t->host);
 		if (w_width)
 			addch(' ');
-		if (t->duplicate != NULL)
-			mvprintw(row, labelwidth, "(duplicate of %s)", t->duplicate->host);
-		else {
-			for (i=ifirst; i<ilast; i++) {
-				if (i < t->npkts)
-					addch(t->res[i % NUM]);
-				else
-					addch(' ');
-			}
+		for (i=ifirst; i<ilast; i++) {
+			if (i < t->npkts)
+				addch(t->res[i % NUM]);
+			else
+				addch(' ');
 		}
 		move(++row, 0);
 	}
@@ -215,11 +211,11 @@ termio_init(void)
 	x = getmaxx();
 	y = getmaxy();
 	if (x > 0 && y > 0) {
-		scrbuffer = malloc(x * y);
-		if (scrbuffer != NULL)
-			setvbuf(stdout, scrbuffer, _IOFBF, x * y);
-		else
-			perror("malloc");
+	scrbuffer = malloc(x * y);
+	if (scrbuffer != NULL)
+		setvbuf(stdout, scrbuffer, _IOFBF, x * y);
+	else
+		perror("malloc");
 	} else {
 		scrbuffer = NULL;
 	}
@@ -317,11 +313,11 @@ termio_cleanup(void)
 
 	endwin();
 	DL_FOREACH(list, t) {
-		if (C_flag && t->dnstask && sa(t)->sa_family == AF_INET6)
+		if (C_flag && t->af == AF_INET6)
 			fprintf(stdout, "%c[2;32m%*.*s%c[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
-		else if (C_flag && t->dnstask && sa(t)->sa_family == AF_INET)
-			fprintf(stdout, "%c[2;31m%*.*s%c[0m",
+		else if (C_flag && t->af == AF_INET)
+			fprintf(stdout, "%c[2;31m%*.*%sc[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
 		else
 			fprintf(stdout, "%*.*s", w_width, w_width, t->host);

http.c
@@ -19,21 +19,26 @@
 #include <event2/util.h>
 #include "xping.h"
 
-static regex_t re_target;
-static struct timeval tv_timeout;
-static void session_eventcb(struct bufferevent *, short, void *);
-static void session_readcb_drain(struct bufferevent *, void *);
+struct probe {
+	char		host[MAXHOST];
+	int		resolved;
+	union addr	sa;
+	char		query[64];
+	void		*owner;
+};
 
-/* state for active tcp connection with a server */
 struct session {
-	struct target	*t;
+	struct probe	*t;
 	int		seq;
 	struct bufferevent *bev;
-
 	struct event	*ev_timeout;
 	int		completed;
 };
 
+static regex_t re_target;
+static struct timeval tv_timeout;
+static void session_eventcb(struct bufferevent *, short, void *);
+static void session_readcb_drain(struct bufferevent *, void *);
 
 /*
  * Drop a session and free the associated state. bufferevent_free is
@@ -106,7 +111,7 @@ session_readcb_status(struct bufferevent *bev, void *thunk)
 	if (atoi(number) < 400 && atoi(number) >= 200 ) {
 		session->completed = 1;
 	} else {
-		target_mark(session->t, session->seq, '%');
+		target_mark(session->t->owner, session->seq, '%');
 	}
 	/* Drain the response on future callbacks */
 	bufferevent_setcb(session->bev, session_readcb_drain, NULL,
@@ -142,16 +147,16 @@ session_eventcb(struct bufferevent *bev, short what, void *thunk)
 	case BEV_EVENT_EOF:
 		bufferevent_disable(bev, EV_READ|EV_WRITE);
 		if (session->completed)
-			target_mark(session->t, session->seq, '.');
+			target_mark(session->t->owner, session->seq, '.');
 		else
-			target_mark(session->t, session->seq, '%');
+			target_mark(session->t->owner, session->seq, '%');
 		break;
 	case BEV_EVENT_ERROR:
 		bufferevent_disable(bev, EV_READ|EV_WRITE);
-		target_mark(session->t, session->seq, '#');
+		target_mark(session->t->owner, session->seq, '#');
 		break;
 	case BEV_EVENT_TIMEOUT:
-		target_mark(session->t, session->seq, '?');
+		target_mark(session->t->owner, session->seq, '?');
 		break;
 	}
 	session_free(session);
@@ -175,7 +180,7 @@ session_timeout(int fd, short what, void *thunk)
 static void
 resolved(int af, void *address, void *thunk)
 {
-	struct target *t = thunk;
+	struct probe *t = thunk;
 	if (af == AF_INET6) {
 		sin6(t)->sin6_family = AF_INET6;
 		memmove(&sin6(t)->sin6_addr, (struct in6_addr *)address,
@@ -189,7 +194,7 @@ resolved(int af, void *address, void *thunk)
 	} else if (af == 0) {
 		t->resolved = 0;
 	}
-	target_resolved(t, af, address);
+	target_resolved(t->owner, af, address);
 }
 
 /*
@@ -262,10 +267,10 @@ static const char uri_chars[256] = {
  * Allocate structure for a new target and parse source line into the
  * structure. Report errors on stderr.
  */
-struct target *
-probe_add(const char *line)
+struct probe *
+probe_new(const char *line, void *owner)
 {
-	struct target *t;
+	struct probe *t;
 	union addr sa;
 	int salen;
 	int port;
@@ -284,7 +289,7 @@ probe_add(const char *line)
 		perror("probe_add: calloc");
 		return (t);
 	}
-	memset(t->res, ' ', sizeof(t->res));
+	t->owner = owner;
 
 	strncat(t->host, line + match[RE_HOST].rm_so,
 	    MIN(sizeof(t->host) - 1,
@@ -358,23 +363,26 @@ probe_add(const char *line)
 		}
 	}
 	sin(t)->sin_port = htons(port);
-	DL_APPEND(list, t);
 	return (t);
 }
 
 /*
  * Allocate session state for a single target probe and launch the probe.
  */
-void probe_send(struct target *t, int seq)
+void probe_send(struct probe *t, int seq)
 {
 	struct session *session;
 	char buf[512];
 	int salen;
 
-	target_unmark(t, seq);
+	target_unmark(t->owner, seq);
+	if (!t->resolved) {
+		target_mark(t->owner, seq, '@');
+		return;
+	}
 	session = calloc(1, sizeof(*session));
 	if (session == NULL) {
-		target_mark(t, seq, '!');
+		target_mark(t->owner, seq, '!');
 		return;
 	}
 	session->t = t;
@@ -382,7 +390,7 @@ void probe_send(struct target *t, int seq)
 	session->bev = bufferevent_socket_new(ev_base, -1,
 	    BEV_OPT_CLOSE_ON_FREE);
 	if (session->bev == NULL) {
-		target_mark(t, seq, '!');
+		target_mark(t->owner, seq, '!');
 		free(session);
 		return;
 	}
@@ -393,7 +401,7 @@ void probe_send(struct target *t, int seq)
 	salen = sa(t)->sa_family == AF_INET6 ? sizeof(struct sockaddr_in6) :
 	    sizeof(struct sockaddr_in);
 	if (bufferevent_socket_connect(session->bev, sa(t), salen) < 0) {
-		target_mark(t, seq, '!');
+		target_mark(t->owner, seq, '!');
 		return;
 	}
 	bufferevent_setwatermark(session->bev, EV_READ, 0, 4096);
@@ -401,7 +409,7 @@ void probe_send(struct target *t, int seq)
 	session->ev_timeout = event_new(ev_base, -1, 0, session_timeout,
 	    session);
 	if (session->ev_timeout == NULL) {
-		target_mark(t, seq, '!');
+		target_mark(t->owner, seq, '!');
 		session_free(session);
 		return;
 	}

icmp-unpriv.c
@@ -14,15 +14,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 #include <event2/event.h>
 #include <event2/buffer.h>
-
 #include "xping.h"
+
+struct probe {
+	char		host[MAXHOST];
+	int		resolved;
+	union addr	sa;
+	int		pid;
+	int		fd;
+	int		seqdelta;
+	int		seqlast;
+	struct evbuffer	*evbuf;
+	void		*owner;
+};
 
 static regex_t re_reply, re_other, re_xmiterr;
 
 static void
-killping(struct target *t)
+killping(struct probe *t)
 {
 	if (t->pid && kill(t->pid, 0) == 0) {
 		kill(t->pid, SIGTERM);
@@ -31,7 +43,7 @@ killping(struct target *t)
 }
 
 static void
-execping(struct target *t)
+execping(struct probe *t)
 {
 	char address[64];
 	char interval[24];
@@ -51,7 +63,7 @@ execping(struct target *t)
 static void
 readping(int fd, short what, void *thunk)
 {
-	struct target *t = thunk;
+	struct probe *t = thunk;
 	regmatch_t match[5];
 	struct evbuffer_ptr evbufptr;
 	double rtt;
@@ -82,14 +94,14 @@ readping(int fd, short what, void *thunk)
 		} else if (regexec(&re_xmiterr, buf, 5, match, 0) == 0) {
 			/* Transmit errors are quickly identified,
 			 * thus assume they refer to most recent packet */
-			target_mark(t, t->seqlast, '!');
+			target_mark(t->owner, t->seqlast, '!');
 		}
 		if (mark != '\0') {
 			/* Adjust sequence adjustment delta. In cast the
 			 * first packet has icmp_seq=0 instead of 1 */
 			if (t->seqlast < 32768 && seq == 0)
 				t->seqdelta++;
-			target_mark(t, seq + t->seqdelta, mark);
+			target_mark(t->owner, seq + t->seqdelta, mark);
 
 			/* Check for timing drift: If packet isn't the
 			 * latest reply and ping thinks rtt is less than
@@ -98,7 +110,7 @@ readping(int fd, short what, void *thunk)
 			    seq + t->seqdelta < t->seqlast &&
 			    rtt < i_interval) {
 				killping(t);
-				target_mark(t, t->seqlast, '!');
+				target_mark(t->owner, t->seqlast, '!');
 			}
 		}
 		evbufptr = evbuffer_search_eol(t->evbuf, NULL, &len, EVBUFFER_EOL_LF);
@@ -113,7 +125,7 @@ readping(int fd, short what, void *thunk)
 static void
 resolved(int af, void *address, void *thunk)
 {
-	struct target *t = thunk;
+	struct probe *t = thunk;
 	if (af == AF_INET6) {
 		if (sin6(t)->sin6_family == AF_INET6 &&
 		    memcmp(&sin6(t)->sin6_addr, (struct in6_addr *)address,
@@ -138,7 +150,7 @@ resolved(int af, void *address, void *thunk)
 		t->resolved = 0;
 		killping(t);
 	}
-	target_resolved(t, af, address);
+	target_resolved(t->owner, af, address);
 }
 
 /*
@@ -181,27 +193,25 @@ probe_setup(struct event_base *parent_event_base)
 	}
 }
 
-struct target *
-probe_add(const char *line)
+struct probe *
+probe_new(const char *line, void *owner)
 {
-	struct target *t;
+	struct probe *t;
 	union addr sa;
 	int salen;
 
-	t = malloc(sizeof(*t));
+	t = calloc(1, sizeof(*t));
 	if (t == NULL) {
 		perror("malloc");
 		return (NULL);
 	}
-	memset(t, 0, sizeof(*t));
-	memset(t->res, ' ', sizeof(t->res));
+	t->owner = owner;
 	strncat(t->host, line, sizeof(t->host) - 1);
 	t->evbuf = evbuffer_new();
 	if (t->evbuf == NULL) {
 		free(t);
 		return (NULL);
 	}
-	DL_APPEND(list, t);
 
 	salen = sizeof(sa);
 	if (evutil_parse_sockaddr_port(t->host, &sa.sa, &salen) == 0) {
@@ -224,11 +234,16 @@ probe_add(const char *line)
 }
 
 void
-probe_send(struct target *t, int seq)
+probe_send(struct probe *t, int seq)
 {
 	struct event *ev;
 	int pair[2];
 	pid_t pid;
+
+	if (!t->resolved) {
+		target_mark(t->owner, seq, '@');
+		return;
+	}
 
 	/* Save most recent sequence number to close gap between
 	 * probe_send returning (target_probe increasing npkts) and forked
@@ -237,7 +252,7 @@ probe_send(struct target *t, int seq)
 
 	/* Clear ahead to avoid overwriting a result in case of small
 	 * timing indiscrepancies */
-	target_unmark(t, seq+1);
+	target_unmark(t->owner, seq+1);
 
 	/* Check for existing ping process */
 	if (t->pid && kill(t->pid, 0) == 0)
@@ -245,7 +260,7 @@ probe_send(struct target *t, int seq)
 
 	/* Create ipc socket pair and fork ping process */
 	if (evutil_socketpair(AF_UNIX, SOCK_STREAM, 0, pair) < 0) {
-		target_mark(t, seq, '!'); /* transmit error */
+		target_mark(t->owner, seq, '!'); /* transmit error */
 		return;
 	}
 	t->seqdelta = seq - 1; /* linux ping(8) begins icmp_seq=1 */
@@ -254,7 +269,7 @@ probe_send(struct target *t, int seq)
 	event_add(ev, NULL);
 	switch (pid = fork()) {
 	case -1:
-		target_mark(t, seq, '!'); /* transmit error */
+		target_mark(t->owner, seq, '!'); /* transmit error */
 		return;
 	case 0:
 		evutil_closesocket(pair[0]);

icmp.c
@@ -20,19 +20,29 @@
 #include <unistd.h>
 
 #include <event2/event.h>
-
 #include "xping.h"
 
 #define ICMP6_MINLEN sizeof(struct icmp6_hdr)
 
-struct target *hash = NULL;
+struct probe {
+	char		host[MAXHOST];
+	int		resolved;
+	union addr	sa;
+	struct probe	*duplicate;
+	int		last_seq;
+	UT_hash_handle	hh;
+	void		*owner;
+};
+
+struct probe *hash = NULL;
 char	outpacket[IP_MAXPACKET];
 char	outpacket6[IP_MAXPACKET];
 int	datalen = 56;
 int	ident;
 
-/* From the original ping.c by Mike Muus... */
 /*
+ * From the original ping.c by Mike Muus...
+ *
  * in_cksum --
  *      Checksum routine for Internet Protocol family headers (C Version)
  */
@@ -80,9 +90,9 @@ in_cksum(u_short *addr, int len)
  * key already exists.
  */
 void
-activate(struct target *t)
+activate(struct probe *t)
 {
-	struct target *result;
+	struct probe *result;
 
 	HASH_FIND(hh, hash, &t->sa, sizeof(union addr), result);
 	if (result == t)
@@ -100,16 +110,16 @@ activate(struct target *t)
  * one (t) instead.
  */
 void
-deactivate(struct target *t)
+deactivate(struct probe *t)
 {
-	struct target *tmp, *t1;
+	struct probe *tmp, *tmp2, *t1;
 
 	HASH_FIND(hh, hash, &t->sa, sizeof(union addr), tmp);
 	if (tmp == NULL)
 		return; /* already inactive, i.e. not in hash */
 	HASH_DELETE(hh, hash, t);
 	t1 = NULL;
-	DL_FOREACH(list, tmp) {
+	HASH_ITER(hh, hash, tmp, tmp2) {
 		if (tmp->duplicate == t) {
 			if (t1 == NULL) {
 				t1 = tmp;
@@ -125,10 +135,10 @@ deactivate(struct target *t)
 /*
  * Lookup target in the hash table
  */
-struct target *
+struct probe *
 find(int af, void *address)
 {
-	struct target *result;
+	struct probe *result;
 	union addr sa;
 
 	memset(&sa, 0, sizeof(sa));
@@ -190,21 +200,22 @@ write_packet6(struct sockaddr *sa, unsigned short seq)
 }
 
 /*
- * find
+ * Find probe target from address and expand truncated icmp_seq from
+ * last sent sequence number.
  */
 static void
 find_marktarget(int af, void *address, int seq, int ch)
 {
-	struct target *t;
+	struct probe *t;
 	int npkts;
 
 	t = find(af, address);
 	if (t == NULL)
 		return; /* unknown source address */
-	npkts = t->npkts;
+	npkts = t->last_seq;
 	if ((npkts & 0xffff) < seq)
-	    npkts--;
-	target_mark(t, (npkts & ~0xffff) | seq, ch);
+	    npkts -= 1<<16;
+	target_mark(t->owner, (npkts & ~0xffff) | seq, ch);
 }
 
 /*
@@ -338,7 +349,7 @@ read_packet6(int fd, short what, void *thunk)
 static void
 resolved(int af, void *address, void *thunk)
 {
-	struct target *t = thunk;
+	struct probe *t = thunk;
 	if (af == AF_INET6) {
 		sin6(t)->sin6_family = AF_INET6;
 		memmove(&sin6(t)->sin6_addr, (struct in6_addr *)address,
@@ -355,7 +366,7 @@ resolved(int af, void *address, void *thunk)
 		t->resolved = 0;
 		deactivate(t);
 	}
-	target_resolved(t, af, address);
+	target_resolved(t->owner, af, address);
 }
 
 /*
@@ -397,22 +408,20 @@ probe_setup()
  * Allocate structure for a new target and insert into list of all
  * our targets.
  */
-struct target *
-probe_add(const char *line)
+struct probe *
+probe_new(const char *line, void *owner)
 {
 	union addr sa;
-	struct target *t;
+	struct probe *t;
 	int salen;
 
-	t = malloc(sizeof(*t));
+	t = calloc(1, sizeof(*t));
 	if (t == NULL) {
 		perror("malloc");
 		return (t);
 	}
-	memset(t, 0, sizeof(*t));
-	memset(t->res, ' ', sizeof(t->res));
+	t->owner = owner;
 	strncat(t->host, line, sizeof(t->host) - 1);
-	DL_APPEND(list, t);
 
 	salen = sizeof(sa);
 	if (evutil_parse_sockaddr_port(t->host, &sa.sa, &salen) == 0) {
@@ -439,10 +448,21 @@ probe_add(const char *line)
  * Send out a single probe for a target.
  */
 void
-probe_send(struct target *t, int seq)
+probe_send(struct probe *t, int seq)
 {
 	int len;
 	int n;
+
+	t->last_seq = seq;
+	if (!t->resolved) {
+		target_mark(t->owner, seq, '@');
+		return;
+	}
+
+	if (t->duplicate) {
+		target_mark(t->owner, seq, '"'); /* transmit error */
+		return;
+	}
 
 	if (sa(t)->sa_family == AF_INET6) {
 		n = write_packet6(sa(t), seq & 0xffff);
@@ -451,11 +471,11 @@ probe_send(struct target *t, int seq)
 		n = write_packet4(sa(t), seq & 0xffff);
 		len = ICMP_MINLEN + datalen;
 	}
-	target_unmark(t, seq);
+	target_unmark(t->owner, seq);
 
 	if (n < 0) {
-		target_mark(t, seq, '!'); /* transmit error */
+		target_mark(t->owner, seq, '!'); /* transmit error */
 	} else if (n != len) {
-		target_mark(t, seq, '$'); /* partial transmit */
+		target_mark(t->owner, seq, '$'); /* partial transmit */
 	}
 }

xping.c
@@ -82,19 +82,6 @@ target_probe(int fd, short what, void *thunk)
 		return;
 	}
 
-	/* Skip any duplicate targets */
-	if (t->duplicate != NULL) {
-		t->npkts++;
-		return;
-	}
-
-	/* Unresolved request */
-	if (!t->resolved) {
-		t->npkts++;
-		target_mark(t, t->npkts - 1, '@');
-		return;
-	}
-
 	/* Missed request */
 	if (t->npkts > 0 && GETRES(t, -1) != '.') {
 		if (GETRES(t, -1) == ' ')
@@ -110,7 +97,7 @@ target_probe(int fd, short what, void *thunk)
 	}
 
 	/* Transmit request */
-	probe_send(t, t->npkts);
+	probe_send(t->pcb, t->npkts);
 	t->npkts++;
 
 	ui_update(t);
@@ -183,8 +170,14 @@ target_add(const char *line)
 {
 	struct target *t;
 
-	t = probe_add(line);
+	t = (struct target *)calloc(1, sizeof(*t));
 	if (t == NULL)
+		return -1;
+	memset(t->res, ' ', sizeof(t->res));
+	strncat(t->host, line, sizeof(t->host) - 1);
+	DL_APPEND(list, t);
+	t->pcb = probe_new(line, t);
+	if (t->pcb == NULL)
 		return -1;
 	numtargets++;
 	return 0;

xping.h
@@ -11,6 +11,7 @@
 #include "utlist.h"
 
 #define NUM 300
+#define MAXHOST 64
 
 extern struct event_base *ev_base;
 extern struct target *list;
@@ -28,31 +29,15 @@ union addr {
 
 struct target {
 	char		host[64];
-	int		resolved;
-
-	union addr	sa;
 	int		npkts;
 	char		res[NUM+1];
 
-	struct dnstask	*dnstask;
+	struct probe	*pcb;
 	struct event	*ev_write;
-	struct target	*duplicate;
 
 	int		row;
 	int		af;
 
-	/* icmp-unpriv. */
-	int		pid;
-	int		fd;
-	int		seqdelta;
-	int		seqlast;
-	struct evbuffer	*evbuf;
-
-	/* http */
-	unsigned short	port;
-	char		query[64];
-
-	UT_hash_handle	hh;
 	struct target	*prev, *next;
 };
 
@@ -80,8 +65,8 @@ void report_cleanup(void);
 
 /* from icmp.c */
 void probe_setup();
-struct target *probe_add(const char *);
-void probe_send(struct target *, int);
+struct probe *probe_new(const char *, void *);
+void probe_send(struct probe *, int);
 
 /* from dnstask.c */
 typedef void (*dnstask_cb_type)(int, void *, void *);

commit bdc0c4a0f18fbb0f54c84f39affde02eb296da73

ui: fix ncurses errors
Fix NCURSES errors introduced in commit 94f8626 (main: split struct
target into main/module parts).

termio.c
@@ -369,21 +369,17 @@ termio_cleanup(void)
 			fprintf(stdout, "%c[2;32m%*.*s%c[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
 		else if (C_flag && t->af == AF_INET)
-			fprintf(stdout, "%c[2;31m%*.*%sc[0m",
+			fprintf(stdout, "%c[2;31m%*.*s%c[0m",
 			    0x1b, w_width, w_width, t->host, 0x1b);
 		else
 			fprintf(stdout, "%*.*s", w_width, w_width, t->host);
 		if (w_width)
 			fputc(' ', stdout);
-		if (t->duplicate != NULL)
-			fprintf(stdout, "(duplicate of %s)", t->duplicate->host);
-		else {
-			for (i=ifirst; i<ilast; i++) {
-				if (i < t->npkts)
-					fputc(t->res[i % NUM], stdout);
-				else
-					fputc(' ', stdout);
-			}
+		for (i=ifirst; i<ilast; i++) {
+			if (i < t->npkts)
+				fputc(t->res[i % NUM], stdout);
+			else
+				fputc(' ', stdout);
 		}
 		fputc('\n', stdout);
 	}